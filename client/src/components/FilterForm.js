b4a2e11b73310d3e9b37aee77d7a26895ddcea38
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-11-06 21:53:29 +0100

    Added form-logic and created a filter-function

--- client/src/components/FilterForm.js
+++ client/src/components/FilterForm.js
@@ -1,30 +1,68 @@
-const FilterForm = (tracks) => {
-  //Get all the unique artists from the tracks
+/*TODO:
+//Fetch artists and bpm
+//map artists genres
+filter on genres and bpm
+*/
 
+const FilterForm = ({ tracks }) => {
+  //Get all the unique artists from the tracks
   const getUniqueArtists = () => [
-    ...new Set(tracks.tracks.map((track) => track.track.artists[0].name)),
+    ...new Set(tracks.map((track) => track.track.artists[0].name)),
   ];
 
+  const getUniqueGenres = () => [];
+
+  //Get the selected filters from the form
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted");
+    const inputObj = {};
+    for (const node of e.target) {
+      if (node.name != null && node.name !== "undefined" && node.value !== "") {
+        inputObj[node.name] = node.value;
+      }
+    }
+
+    filterTracks(inputObj);
+  };
+
+  //Filter tracks with inputs from the form
+  const filterTracks = (filters) => {
+    let filteredTracks = [...tracks];
+
+    if (filters.artist) {
+      filteredTracks = filteredTracks.filter(
+        (track) => track.track.artists[0].name === filters.artist
+      );
+    }
+
+    if (filters.min_duration && filters.max_duration) {
+      filteredTracks = filteredTracks.filter(
+        (track) =>
+          Number(filters.min_duration) * 60 * 1000 <= track.track.duration_ms &&
+          Number(filters.max_duration) * 60 * 1000 >= track.track.duration_ms
+      );
+    }
+
+    console.log(filteredTracks);
   };
 
   return (
     <form className="filter-form" onSubmit={handleSubmit}>
       <div>
         <label>Genre:</label>
-        <select id="genre-select">
-          <option>rock</option>
-          <option>pop</option>
-          <option>house</option>
-          <option>country</option>
+        <select name="genre" id="genre-select">
+          {getUniqueGenres().map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
         </select>
       </div>
 
       <div>
         <label>Artist:</label>
-        <select id="artist-select">
+        <select name="artist" id="artist-select">
+          <option></option>
           {getUniqueArtists().map((artist, index) => (
             <option key={index} value={artist}>
               {artist}
@@ -35,14 +73,14 @@ const FilterForm = (tracks) => {
 
       <div>
         <label>Duration</label>
-        <input type="number"></input>
-        <input type="number"></input>
+        <input name="min_duration" type="number"></input>
+        <input name="max_duration" type="number"></input>
       </div>
 
       <div>
         <label>BPM:</label>
-        <input type="number"></input>
-        <input type="number"></input>
+        <input name="min_bpm" type="number"></input>
+        <input name="max_bpm" type="number"></input>
       </div>
 
       <button>Filter</button>
